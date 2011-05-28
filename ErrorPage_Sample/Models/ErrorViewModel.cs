01e36793a64f455cf5e196e22348eb38ab94ccad
Ferdinand Lukasak <feroalien@hotmail.com>
2011-05-28 18:53:27 +1000

    Initial Commit

---
+++ ErrorPage_Sample/Models/ErrorViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using ErrorPage_Sample.Views;
+
+
+namespace ErrorPage_Sample.Models
+{
+    public class ErrorViewModel : INotifyPropertyChanged
+    {
+        private Exception _exception;
+        private string _versionInformation;
+
+        public ErrorViewModel(Exception exception, string versionInformation)
+        {
+            Exception = exception;
+            VersionInformation = versionInformation;
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public Exception Exception
+        {
+            get { return _exception; }
+            private set
+            {
+                if (_exception != value)
+                {
+                    _exception = value;
+                    OnPropertyChanged("Exception");
+                }
+            }
+        }
+
+        public string VersionInformation
+        {
+            get { return _versionInformation; }
+            private set
+            {
+                if (_versionInformation != value)
+                {
+                    _versionInformation = value;
+                    OnPropertyChanged("VersionInformation");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ ErrorPage_Sample/Views/ErrorView.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using ErrorPage_Sample.Models;
+
+
+namespace ErrorPage_Sample.Views
+{
+    public partial class ErrorView : UserControl
+    {
+        public ErrorView()
+        {
+            InitializeComponent();
+
+            this.DataContext = new ErrorViewModel(GenerateException(), "Sample Version: 1.0, Is Release Version : false");
+        }
+
+        private Exception GenerateException()
+        {
+            Exception exception = null;
+            try
+            {
+                int i = 0;
+                int result = 10 / i;
+
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+            return exception;
+        }
+    }
+}
